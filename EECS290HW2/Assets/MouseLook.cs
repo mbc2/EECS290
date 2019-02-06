7b3783140049604153c5163e11c17f88f7f949f6
mbc2 <42946248+mbc2@users.noreply.github.com>
2019-02-06 18:55:51 -0500

    commit

---
+++ EECS290HW2/Assets/MouseLook.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float horSensitivity = 8.0f;
+    public float verSensitivity = 8.0f;
+
+    public float verMin = -45.0f;
+    public float verMax = 45.0f;
+
+    private float _rotationX = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * horSensitivity, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * verSensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, verMin, verMax);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * verSensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, verMin, verMax);
+
+            float delta = Input.GetAxis("Mouse X") * horSensitivity;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+		
+	}
+}
